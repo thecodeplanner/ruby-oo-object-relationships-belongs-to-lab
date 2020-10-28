ac4e32ec1d8ad4e0fb9e0f5ce4199c48a408df64
thecodeplanner <thecodeplanner@gmail.com>
2020-10-28 19:58:27 -0400

    finished

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+# require_relative "../lib/song.rb"
+
+class Artist 
+    attr_accessor :name 
+    def initialize
+        @name = name 
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/artist.rb"
+
+class Song
+    attr_accessor :title, :artist 
+    def initialize
+        @title = title
+    end
+end
